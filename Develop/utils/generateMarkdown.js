e6bb32ef4e41e131bc1bcf45ce98a37572f11279
Tsunwei514 <tristin.tsun@gmail.com>
2024-12-05 05:38:31 -0700

    add badge function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,28 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) { 
+  if (license === 'MIT License') {
+    return 'https://img.shields.io/badge/License-MIT-yellow.svg';
+  } else if (license === 'Apache License 2.0') {
+    return 'https://img.shields.io/badge/License-Apache%202.0-blue.svg';
+  } else if (license === 'GNU General Public License v3.0 (GPL-3.0)') {
+    return 'https://img.shields.io/badge/License-GPL--3.0-blue.svg';
+  } else if (license === 'BSD 3-Clause License') {
+    return 'https://img.shields.io/badge/License-BSD%203--Clause-blue.svg';
+  } else if (license === 'GNU Lesser General Public License v3.0 (LGPL-3.0)') {
+    return 'https://img.shields.io/badge/License-LGPL--3.0-blue.svg';
+  } else if (license === 'Creative Commons Zero (CC0)') {
+    return 'https://img.shields.io/badge/License-CC0-blue.svg';
+  } else if (license === 'Mozilla Public License 2.0 (MPL-2.0)') {
+    return 'https://img.shields.io/badge/License-MPL--2.0-brightgreen.svg';
+  } else if (license === 'Eclipse Public License 2.0 (EPL-2.0)') {
+    return 'https://img.shields.io/badge/License-EPL--2.0-blue.svg';
+  } else if (license === 'Unlicense') {
+    return 'https://img.shields.io/badge/license-Unlicense-blue.svg';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
